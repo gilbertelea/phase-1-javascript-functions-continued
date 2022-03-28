54429ae6fde72a244f5afa19ff715427f58ebc14
gilbert <eleazarbiboum@gmail.com>
2022-03-27 21:13:57 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+
+function saturdayFun(ar1 = "roller-skate") {
+    
+  return (`This Saturday, I want to ${ar1}!`);
+}
+saturdayFun(ar1);
+saturdayFun();
+
+
+function mondayWork(ar2 = "go to the office"){
+
+  return (`This Monday, I will ${ar2}.`)
+    }
+
+mondayWork(ar2);
+mondayWork();
+/*let result = "*"
+let emphatic = result("a hard worker"); */
+
+function wrapAdjective(result = "*"){
+   const adj = function(emphatic = "special"){
+    return `You are ${result}${emphatic}${result}!`
+}
+    return adj;
+}
